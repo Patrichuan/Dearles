e62dd709cd82595a2cad1fd00ef158b3dcc79d2d
Patrichuan <Patrichuan@gmail.com>
2015-08-04 18:58:26 +0200

    - Floating label overlap bug SOLVED !

--- app/src/main/java/dear/dearles/activities/SignUp1.java
+++ app/src/main/java/dear/dearles/activities/SignUp1.java
@@ -1,24 +1,18 @@
 package dear.dearles.activities;
 
-import android.app.Activity;
-import android.content.ContentResolver;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
 import android.text.InputFilter;
 import android.text.InputType;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,15 +22,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.apache.http.client.UserTokenHandler;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-
-import bolts.Bolts;
 import dear.dearles.DearApp;
 import dear.dearles.R;
 
@@ -50,6 +35,12 @@ public class SignUp1 extends AppCompatActivity {
 
     private ImageView ProfilePictureView;
 
+    //keep track of camera capture intent
+    final int CAMERA_CAPTURE = 1;
+    //captured picture uri
+    private Uri picUri;
+    //keep track of cropping intent
+    final int PIC_CROP = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +54,7 @@ public class SignUp1 extends AppCompatActivity {
         emailTil = (TextInputLayout) findViewById(R.id.emailTil);
         ageTil = (TextInputLayout) findViewById(R.id.ageTil);
         passwordTil = (TextInputLayout) findViewById(R.id.passwordTil);
+        ProfilePictureView = (ImageView) findViewById(R.id.SignUpProfileiv);
 
         app = (DearApp) getApplication();
 
@@ -86,29 +78,75 @@ public class SignUp1 extends AppCompatActivity {
         });
 
 
-        ProfilePictureView = (ImageView) findViewById(R.id.SignUpProfileiv);
+
         ProfilePictureView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+                try {
+                    //use standard intent to capture an image
+                    Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    //we will handle the returned data in onActivityResult
+                    startActivityForResult(captureIntent, CAMERA_CAPTURE);
+                } catch(ActivityNotFoundException anfe){
+                    //display an error message
+                    String errorMessage = "Whoops - your device doesn't support capturing images!";
+                    Toast toast = Toast.makeText(SignUp1.this, errorMessage, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
     }
 
 
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Bitmap bp = (Bitmap) data.getExtras().get("data");
-        // Este bp es el que habria que guardar al final
-        ProfilePictureView.setImageBitmap(bp);
+        if (resultCode == RESULT_OK) {
+            //user is returning from capturing an image using the camera
+            if(requestCode == CAMERA_CAPTURE){
+                //get the Uri for the captured image
+                picUri = data.getData();
+                //carry out the crop operation
+                performCrop();
+            //user is returning from cropping the image
+            } else if (requestCode == PIC_CROP) {
+                //get the returned data
+                Bundle extras = data.getExtras();
+                //get the cropped bitmap
+                Bitmap thePic = extras.getParcelable("data");
+                //retrieve a reference to the ImageView
+                ImageView picView = (ImageView)findViewById(R.id.SignUpProfileiv);
+                //display the returned cropped image
+                picView.setImageBitmap(thePic);
+            }
+        }
     }
 
-
-
+    // peligro
+    private void performCrop(){
+        try {
+            //call the standard crop action intent (the user device may not support it)
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            //indicate image type and Uri
+            cropIntent.setDataAndType(picUri, "image/*");
+            //set crop properties
+            cropIntent.putExtra("crop", "true");
+            //indicate aspect of desired crop
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            //indicate output X and Y
+            cropIntent.putExtra("outputX", 256);
+            cropIntent.putExtra("outputY", 256);
+            //retrieve data on return
+            cropIntent.putExtra("return-data", true);
+            //start the activity - we handle returning in onActivityResult
+            startActivityForResult(cropIntent, PIC_CROP);
+        }
+        catch(ActivityNotFoundException anfe){
+            //display an error message
+            String errorMessage = "Whoops - your device doesn't support the crop action!";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
 
 
     private void setupToolbar(){
@@ -120,8 +158,6 @@ public class SignUp1 extends AppCompatActivity {
         ab.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_36dp);
         ab.setDisplayHomeAsUpEnabled(true);
     }
-
-
 
 
     private void setupEditTexts () {
