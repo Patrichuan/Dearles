7e3d955191f97028933b0cbdd5eb10f7c2377d30
Patrichuan <Patrichuan@gmail.com>
2015-08-06 23:40:26 +0200

    - Dialog for choose Profile Picture font - DONE !

--- app/src/main/java/dear/dearles/activities/SignUp1.java
+++ app/src/main/java/dear/dearles/activities/SignUp1.java
@@ -1,26 +1,20 @@
 package dear.dearles.activities;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputFilter;
 import android.text.InputType;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -37,17 +31,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
-import com.squareup.picasso.Transformation;
-
-import java.io.File;
-import java.io.FileOutputStream;
 
 import dear.dearles.DearApp;
 import dear.dearles.R;
@@ -60,7 +47,6 @@ public class SignUp1 extends AppCompatActivity {
     Button Nextbtn;
     TextView Username, Password, Email, Age;
     TextInputLayout usernameTil, passwordTil, emailTil, ageTil;
-    Button dialogBtn;
 
     private ImageView ProfilePictureView;
 
@@ -91,7 +77,6 @@ public class SignUp1 extends AppCompatActivity {
         app = (DearApp) getApplication();
         UserData = app.getUserData();
 
-
         ProgressCircle = (ImageView) findViewById(R.id.ProgressCircle);
         a = AnimationUtils.loadAnimation(this, R.anim.progress_anim);
         a.setDuration(1000);
@@ -99,29 +84,6 @@ public class SignUp1 extends AppCompatActivity {
         // Setups
         setupToolbar();
         setupEditTextsAndPicture();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         Nextbtn = (Button) findViewById(R.id.Nextbtn);
         Nextbtn.setOnClickListener(new View.OnClickListener() {
@@ -140,8 +102,6 @@ public class SignUp1 extends AppCompatActivity {
         });
 
 
-
-
         ProfilePictureView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -155,7 +115,6 @@ public class SignUp1 extends AppCompatActivity {
                 d.show();
 
                 Button Camerabtn = (Button) d.findViewById(R.id.Camerabtn);
-                //if button clicked close the dialog
                 Camerabtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -163,7 +122,7 @@ public class SignUp1 extends AppCompatActivity {
                             d.dismiss();
                             Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                             startActivityForResult(captureIntent, CAMERA_CAPTURE);
-                        } catch(ActivityNotFoundException anfe){
+                        } catch(ActivityNotFoundException e){
                             //display an error message
                             String errorMessage = "Whoops - your device doesn't support capturing images!";
                             Toast toast = Toast.makeText(SignUp1.this, errorMessage, Toast.LENGTH_SHORT);
@@ -179,9 +138,9 @@ public class SignUp1 extends AppCompatActivity {
                     public void onClick(View v) {
                         try {
                             d.dismiss();
-                            Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                            startActivityForResult(i, IMAGE_PICKER_SELECT);
-                        } catch(ActivityNotFoundException anfe){
+                            Intent captureIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(captureIntent, IMAGE_PICKER_SELECT);
+                        } catch(ActivityNotFoundException e){
                             //display an error message
                             String errorMessage = "Whoops - your device doesn't support pick images!";
                             Toast toast = Toast.makeText(SignUp1.this, errorMessage, Toast.LENGTH_SHORT);
@@ -194,68 +153,31 @@ public class SignUp1 extends AppCompatActivity {
     }
 
 
+
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if (resultCode == RESULT_OK) {
 
             ProgressCircle.startAnimation(a);
             picUri = data.getData();
-            /*
-            Bundle extras = data.getExtras();
-            Bitmap thePic = extras.getParcelable("data");
 
-            ProfilePictureView.setImageBitmap(thePic);
-            UserData[4]=(picUri.toString());
-
-
-
-
-            //user is returning from capturing an image using the camera
-            if(requestCode == CAMERA_CAPTURE){
-
-                // ESTE BITMAP HAY QUE GUARDARLO PARA LA PAGINA DE PERFIL PANTALLA COMPLETA
-                // ------------------------------------------------------------------------
-                // Bundle extras = data.getExtras();
-                // Bitmap thePic = extras.getParcelable("data");
-                // Drawable drawable = new BitmapDrawable(getApplicationContext().getResources(), thePic);
-
-                Picasso.with(this)
-                        .load(picUri)
-                        .transform(new CropSquareTransformation())
-                        .fit()
-                        .into(ProfilePictureView);
-                UserData[4]=(picUri.toString());
-
-            } else if (requestCode == IMAGE_PICKER_SELECT) {
-
-
-
-                Picasso.with(this)
-                        .load(picUri)
-                        .transform(new CropSquareTransformation())
-                        .into(ProfilePictureView);
-                UserData[4]=(picUri.toString());
-
-            */
-
-
-            //Cache de Glide ----> File cacheFile = Glide.getPhotoCacheDir();
-
-                // Loads given image (LO LEO EN EL IMAGEVIEW)
-                Glide.with(this)
-                        .load(picUri)
-                        .asBitmap()
-                        .transform(new CropSquareTransformation(this))
-                        .into(new SimpleTarget<Bitmap>() {
-                            @Override
-                            public void onResourceReady(final Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
-                                UserData[4] = picUri.toString();
-                                ProgressCircle.clearAnimation();
-                                ProfilePictureView.setImageBitmap(resource);
-                            }
-                        });
+            // Loads given image (LO LEO EN EL IMAGEVIEW)
+            Glide.with(this)
+                    .load(picUri)
+                    .asBitmap()
+                    .transform(new CropSquareTransformation(this))
+                    .into(new SimpleTarget<Bitmap>() {
+                        @Override
+                        public void onResourceReady(final Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                            UserData[4] = picUri.toString();
+                            ProgressCircle.clearAnimation();
+                            ProfilePictureView.setImageBitmap(resource);
+                        }
+                    });
         }
     }
+
 
 
 
@@ -334,6 +256,7 @@ public class SignUp1 extends AppCompatActivity {
             Email.setText(UserData[3]);
         }
 
+
         usernameTil.addView(Username);
         passwordTil.addView(Password);
         ageTil.addView(Age);
@@ -342,7 +265,6 @@ public class SignUp1 extends AppCompatActivity {
 
         if (UserData[4]!=null){
             ProgressCircle.startAnimation(a);
-
             Glide.with(this)
                     .load(Uri.parse(UserData[4]))
                     .asBitmap()
@@ -356,6 +278,7 @@ public class SignUp1 extends AppCompatActivity {
                     });
         }
     }
+
 
 
     // No deja validar hasta rellenar los 4 campos (username, email, age, password)
@@ -375,7 +298,6 @@ public class SignUp1 extends AppCompatActivity {
             UsernameCorrect = true;
         }
 
-
         if (passwordTil.getEditText().getText().toString().equals("")) {
             passwordTil.setError(getString(R.string.password_required));
             passwordTil.setErrorEnabled(true);
@@ -383,7 +305,6 @@ public class SignUp1 extends AppCompatActivity {
             passwordTil.setError("");
             PasswordCorrect = true;
         }
-
 
         if (emailTil.getEditText().getText().toString().equals("")) {
             emailTil.setError(getString(R.string.email_required));
@@ -393,8 +314,6 @@ public class SignUp1 extends AppCompatActivity {
             EmailCorrect = true;
         }
 
-
-        // && Condicion de que sea un numero
         // Falta la validación de la edad (si son digitos o no)
         // Tunear teclado numerico
         if (ageTil.getEditText().getText().toString().equals("")) {
@@ -404,7 +323,6 @@ public class SignUp1 extends AppCompatActivity {
             ageTil.setError("");
             AgeCorrect = true;
         }
-
 
         if (UsernameCorrect && PasswordCorrect && EmailCorrect && AgeCorrect) {
             Correct = true;
@@ -450,14 +368,11 @@ public class SignUp1 extends AppCompatActivity {
 
         @Override
         protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
-
             Bitmap MyBitmapTransformed;
-
             int size = Math.min(toTransform.getWidth(), toTransform.getHeight());
             int x = (toTransform.getWidth() - size) / 2;
             int y = (toTransform.getHeight() - size) / 2;
             MyBitmapTransformed = Bitmap.createBitmap(toTransform, x, y, size, size);
-
             return MyBitmapTransformed;
         }
 
